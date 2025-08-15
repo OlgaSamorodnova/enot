57773f93281fee39b3ab31c00b0a7cc53eda0876
OlgaSamorodnova <118749608+OlgaSamorodnova@users.noreply.github.com>
2025-08-15 23:54:54 +0300

    Update yclients.js

--- api/_lib/yclients.js
+++ api/_lib/yclients.js
@@ -13,7 +13,7 @@ export async function getRecordsByPhone(phone) {
     throw new Error('Не указан YCLIENTS_COMPANY_ID или YCLIENTS_BEARER');
   }
 
-  const url = `${BASE}/companies/${companyId}/visits?phone=${normalized}&future=1`;
+  const url = `${BASE}/companies/${companyId}/visits/search/?client_phone=${normalized}&future=1`;
 
   const resp = await fetch(url, {
     headers: {
